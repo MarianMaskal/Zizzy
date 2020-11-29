3e6cac21442865d6225044b3bf1a2e6439e31606
MarianMaskal <32840117+MarianMaskal@users.noreply.github.com>
2020-11-30 00:12:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 $('.card-small__info-showMore').click(function() {
-    // $( "#card-hidden" ).toggleClass( "hidden" );
-    // $( ".card" ).toggleClass( "max-height" );
-    // $('#showMore').hide();
-    // $('#showLess').show();
-    console.log($(this));
     $(this).parents().siblings('.card-hidden').toggleClass( "hidden" );
     $(this).parents('.card').toggleClass( "max-height" );
     $(this).hide();
@@ -11,8 +6,6 @@ $('.card-small__info-showMore').click(function() {
 });
 
 $('.card-big__showMore').click(function() {
-    console.log($(this));
-
     $(this).parents('.card-hidden').toggleClass( "hidden" );
     $(this).parents('.card').toggleClass( "max-height" );
     $(this).hide();
